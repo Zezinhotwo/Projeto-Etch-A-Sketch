c5dd53e4888b628820706fd7150ca2649ab4ffca
Zezinhodois <123456789+kaio.bg12@gmail.com>
2024-03-15 18:46:49 -0300

    removel entrad do usuario tendo um limite de grade pre esbabelecido

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ Crie as divs usando JavaScript.16x16
 Use o flexbox para fazer os divs aparecerem como uma grade
 */
 const container = document.querySelector(".conteiner");
-let tamGrade = Number(prompt("qual proporção você deseja EX'16' = 16X16 "));
+let tamGrade = 50// Number(prompt("qual proporção você deseja EX'16' = 16X16 "));
 if (tamGrade <= 100) {
 
     //Crai um arrei com o numero de Elementos digita do no prompt
